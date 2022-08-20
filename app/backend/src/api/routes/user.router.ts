a160ba881dfda3ae2cb2cf0f928bcc9108185bc4
Silas <silas.oliveira8@gmail.com>
2022-08-20 16:22:30 -0300

    feat: status response change

--- app/backend/src/api/routes/user.router.ts
+++ app/backend/src/api/routes/user.router.ts
@@ -5,7 +5,7 @@ const userRouter = Router();
 
 userRouter.post('/', async (req, res, _next) => {
   const result = await UserController.login(req.body);
-  return res.status(201).json({ token: result });
+  return res.status(200).json({ token: result });
 });
 
 export default userRouter;
