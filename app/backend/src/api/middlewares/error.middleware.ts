aac24d06593875ea30e6bc4e3a79c0df4c761b13
Silas <silas.oliveira8@gmail.com>
2022-08-20 15:56:04 -0300

    feat: erros middleware and throwErrors, created

---
+++ app/backend/src/api/middlewares/error.middleware.ts
@@ -0,0 +1,8 @@
+import { NextFunction, Request, Response } from 'express';
+
+const errorHandler = ({ message }: Error, _req: Request, res: Response, _next: NextFunction) => {
+  const [msg, status, token] = message.split('/');
+  return res.status(Number(status)).json({ [token ? 'error' : 'message']: msg });
+};
+
+export default errorHandler;
---
+++ app/backend/src/utils/throwError/throw.error.ts
@@ -0,0 +1,26 @@
+const throwError = (name: string, defaultMessage = '') => (message = defaultMessage) => {
+  const error = new Error(message);
+  error.name = name;
+  throw error;
+};
+
+// verbos como nome do método === throwUnauthorizedError
+export const throwUnauthorizedError = throwError(
+  'UnauthorizedError',
+  'Invalid fields/400',
+);
+
+export const throwInvalidToken = throwError(
+  'InvalidToken',
+  'Invalid Token/401/token',
+);
+
+export const throwTokenNotFound = throwError(
+  'TokenNotFound',
+  'Token not found/401/token',
+);
+
+export const throwExpiredOrInvalidToken = throwError(
+  'expiredOrInvalidToken',
+  'Expired or invalid token/401/token',
+);
